bea379740b985ade4a9b5c86f720cc26a5532af6
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2024-07-14 22:47:19 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for(let i = 0; i < arr.length; i++)
+    {
+        for(const num of toRemove)
+        {
+            if(num === arr[i])
+            {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
