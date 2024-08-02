6e65368f3f9ed180a6f6accb5fe15ad42f91b8cc
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2024-08-01 21:47:50 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let trimmedString = "";
+    for(let i = 0; i < str.length; i++)
+    {
+        if(str.charAt(i).toUpperCase() !== str.charAt(i).toLowerCase() || parseInt(str.charAt(i)))
+        {
+            trimmedString += str.charAt(i).toLowerCase();
+        }
+    }
+    return trimmedString.split("").reverse().join("") === trimmedString;
 };
 
 // Do not edit below this line
