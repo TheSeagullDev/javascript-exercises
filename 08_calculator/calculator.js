ec4316db15b537c5f236d34976faab73aee25c01
TheSeagullDev <67653706+TheSeagullDev@users.noreply.github.com>
2024-08-01 21:20:10 -0400

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, num) => product *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if(x === 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return x * factorial(x-1);
+  }
 };
 
 // Do not edit below this line
